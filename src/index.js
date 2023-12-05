1019a8b07f2c760c021d36974ba522aba2c5ec1b
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2023-12-05 22:48:12 +0530

    Added create ticket api

--- src/index.js
+++ src/index.js
@@ -13,15 +13,15 @@ app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
   console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-  try {
-    const response = await mailsender.sendMail({
-      from: ServerConfig.GMAIL_EMAIL,
-      to: "xyz@gmail.com",
-      subject: "Is the service working ? now as well",
-      text: "Yes it is working",
-    });
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
+  // try {
+  //   const response = await mailsender.sendMail({
+  //     from: ServerConfig.GMAIL_EMAIL,
+  //     to: "xyz@gmail.com",
+  //     subject: "Is the service working ? now as well",
+  //     text: "Yes it is working",
+  //   });
+  //   console.log(response);
+  // } catch (error) {
+  //   console.log(error);
+  // }
 });
--- src/utils/controllers/index.js
+++ src/utils/controllers/index.js
@@ -1,4 +1,4 @@
 module.exports = {
   InfoController: require("./info-controller"),
-  EmailController: require("./email-controller"),
+  BookingController: require("./booking-controller"),
 };
